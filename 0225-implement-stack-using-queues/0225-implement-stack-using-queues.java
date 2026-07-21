f0bd520c0dc826fe69bb4aacfe0685001c5ce814
Vaishnavi <vaishnavichimkar29@gmail.com>
2026-07-21 21:43:28 +0530

    Time: 1 ms (63.84%), Space: 42.6 MB (90.04%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,40 +1,73 @@
 class MyStack {
+
     Queue<Integer> q1;
     Queue<Integer> q2;
+
     public MyStack() {
         q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q2 = new LinkedList<>();  
     }
     
     public void push(int x) {
-
-        while(!q1.isEmpty()){
-            q2.add(q1.remove());
-        } 
-
-        q1.add(x);
-
-        while(!q2.isEmpty()){
-            q1.add(q2.remove());
-        }  
+        if(!q1.isEmpty()){
+            q1.add(x);
+        }else{
+            q2.add(x);
+        }
     }
     
     public int pop() {
-        if(q1.isEmpty()){
+        
+        if(empty()){
             return -1;
-        }  
-        return q1.remove();  
+        }
+
+        int top = -1;
+        //case 1
+        if(!q1.isEmpty()){
+            while(!q1.isEmpty()){
+                top = q1.remove();
+                if(q1.isEmpty()){
+                    break;
+                }
+                q2.add(top);
+            }
+        }else{//case 2
+            while(!q2.isEmpty()){
+                top = q2.remove();
+                if(q2.isEmpty()){
+                    break;
+                }
+                q1.add(top);
+            }
+        }
+        return top;
     }
     
     public int top() {
-        if(q1.isEmpty()){
+        
+        if(empty()){
             return -1;
-        }  
-        return q1.peek();     
+        }
+
+        int top = -1;
+        //case 1
+        if(!q1.isEmpty()){
+            while(!q1.isEmpty()){
+                top = q1.remove();
+                q2.add(top);
+            }
+        }else{//case 2
+            while(!q2.isEmpty()){
+                top = q2.remove();
+                q1.add(top);
+            }
+        }
+        return top;
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
 
